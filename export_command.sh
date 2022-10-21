6e30f6f8be6dfbd0feb73972c8ed1b01afdf178d
Jeonkwan Chan <jeonkwan@gmail.com>
2022-10-21 23:39:38 +0800

    update script

--- export_command.sh
+++ export_command.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
-source /Users/jeonkwan/python-venvs/photos_processing/bin/activate
-which osxphotos
+export DIR_NAME_TEMPLATE="{created.year}/{created.month}"
+export FILE_NAME_TEMPLATE="[{created.strftime,%Y-%m-%d}][{created.strftime,%H_%M_%S}]{place.country_code?[{place.country_code}],[]}{place.name.city?[{place.name.city}],[]}{exif.camera_model?[{exif.camera_model}],[]}{edited?[EDITED],[ORIGINAL]}"
+export FILE_NAME_TEMPLATE_WITH_SHORT_UUID="[{created.strftime,%Y-%m-%d}][{created.strftime,%H_%M_%S}][{shortuuid}]{place.country_code?[{place.country_code}],[]}{place.name.city?[{place.name.city}],[]}{exif.camera_model?[{exif.camera_model}],[]}{edited?[EDITED],[ORIGINAL]}"
+
 
 get_year_range_flag() {
     local TARGET_YEAR=$1
@@ -24,18 +26,15 @@ get_season_range_flag() {
     [[ $SEASON -eq 4 ]] && { echo "$FOURTH"; return 0; }
 }
 
-
-DIR_NAME_TEMPLATE="{created.year}/{created.month}"
-FILE_NAME_TEMPLATE="[{created.strftime,%Y-%m-%d}][{created.strftime,%H_%M_%S}]{place.country_code?[{place.country_code}],[]}{place.name.city?[{place.name.city}],[]}{exif.camera_model?[{exif.camera_model}],[]}{edited?[EDITED],[ORIGINAL]}"
-FILE_NAME_TEMPLATE_WITH_SHORT_UUID="[{created.strftime,%Y-%m-%d}][{created.strftime,%H_%M_%S}][{shortuuid}]{place.country_code?[{place.country_code}],[]}{place.name.city?[{place.name.city}],[]}{exif.camera_model?[{exif.camera_model}],[]}{edited?[EDITED],[ORIGINAL]}"
-
 photos_export() {
     local TARGET_DIR=$1
     local RANGE_FLAG=$2
     local OUTPUT_DIR_TEMPLATE=$3
     local OUTPUT_NAME_TEMPLATE=$4
 
-    mkdir -p $TARGET_DIR
+    [[ -d "$TARGET_DIR" ]] && { echo "Directory $TARGET_DIR exists."; } || { echo "Error: Directory /path/to/dir does not exists."; return 1; }
+
+    { which osxphotos; } || { echo "EXIT. osxphotos NOT FOUND."; return 1; }
     osxphotos export $TARGET_DIR \
         $RANGE_FLAG \
         --directory $OUTPUT_DIR_TEMPLATE \

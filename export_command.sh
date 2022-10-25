6f79f914604682eb14fd0edad03b0b6ce1ef2a07
Jeonkwan Chan <jeonkwan@gmail.com>
2022-10-26 01:51:06 +0800

    add split by month

--- export_command.sh
+++ export_command.sh
@@ -3,6 +3,12 @@
 export FILE_NAME_TEMPLATE="[{created.strftime,%Y-%m-%d}][{created.strftime,%H_%M_%S}]{place.country_code?[{place.country_code}],[]}{place.name.city?[{place.name.city}],[]}{exif.camera_model?[{exif.camera_model}],[]}{edited?[EDITED],[ORIGINAL]}"
 export FILE_NAME_TEMPLATE_WITH_SHORT_UUID="[{created.strftime,%Y-%m-%d}][{created.strftime,%H_%M_%S}][{shortuuid}]{place.country_code?[{place.country_code}],[]}{place.name.city?[{place.name.city}],[]}{exif.camera_model?[{exif.camera_model}],[]}{edited?[EDITED],[ORIGINAL]}"
 
+get_last_day_of_month() {
+    local MONTH=$1
+    local YEAR=$2
+    cal $MONTH $YEAR | awk 'NF {DAYS = $NF}; END {print DAYS}'
+}
+
 photos_export() {
     set -x
     
@@ -11,6 +17,7 @@ photos_export() {
     local TARGET_DIR=$1
     local YEAR=$2
     local SEASON=$3
+    local SPLIT_BY_MONTH=$4
 
     local OUTPUT_DIR_TEMPLATE="{created.mm}-{created.month}"
     local OUTPUT_NAME_TEMPLATE=$FILE_NAME_TEMPLATE
@@ -38,26 +45,53 @@ photos_export() {
             --report $REPORT_PATH
     else
         echo "\$SEASON is provided. Export by Year Season."
-        [[ $SEASON -eq 1 ]] && { FROM_DATE="${YEAR}-01-01"; TO_DATE="${YEAR}-03-31"; }
-        [[ $SEASON -eq 2 ]] && { FROM_DATE="${YEAR}-04-01"; TO_DATE="${YEAR}-06-30"; }
-        [[ $SEASON -eq 3 ]] && { FROM_DATE="${YEAR}-07-01"; TO_DATE="${YEAR}-09-30"; }
-        [[ $SEASON -eq 4 ]] && { FROM_DATE="${YEAR}-10-01"; TO_DATE="${YEAR}-12-31"; }
+        [[ $SEASON -eq 1 ]] && { FROM_MONTH="01"; TO_MONTH="03"; }
+        [[ $SEASON -eq 2 ]] && { FROM_MONTH="04"; TO_MONTH="06"; }
+        [[ $SEASON -eq 3 ]] && { FROM_MONTH="07"; TO_MONTH="09"; }
+        [[ $SEASON -eq 4 ]] && { FROM_MONTH="10"; TO_MONTH="12"; }
+        local FROM_DATE="${YEAR}-${FROM_MONTH}-01"
+        local TO_MONTH_LAST_DAY="$(get_last_day_of_month $TO_MONTH $YEAR)"
+        local TO_DATE="${YEAR}-${TO_MONTH}-${TO_MONTH_LAST_DAY}"
         local EXPORT_DIR=$TARGET_DIR/${YEAR}-Season-${SEASON}
         REPORT_PATH=$TARGET_DIR/${YEAR}-Season-${SEASON}-report.csv
         mkdir -p $EXPORT_DIR
-        osxphotos export $EXPORT_DIR \
-            --from-date "${FROM_DATE}" \
-            --to-date "${TO_DATE}" \
-            --directory "$OUTPUT_DIR_TEMPLATE" \
-            --filename "$OUTPUT_NAME_TEMPLATE" \
-            --edited-suffix "" \
-            --skip-original-if-edited \
-            --download-missing \
-            --use-photokit \
-            --retry 5 \
-            --update \
-            --cleanup \
-            --report $REPORT_PATH
+        if [[ -z "$SPLIT_BY_MONTH" ]]; then
+            osxphotos export $EXPORT_DIR \
+                --from-date "${FROM_DATE}" \
+                --to-date "${TO_DATE}" \
+                --directory "$OUTPUT_DIR_TEMPLATE" \
+                --filename "$OUTPUT_NAME_TEMPLATE" \
+                --edited-suffix "" \
+                --skip-original-if-edited \
+                --download-missing \
+                --use-photokit \
+                --retry 5 \
+                --update \
+                --cleanup \
+                --report $REPORT_PATH
+        fi
+        if [[ "$SPLIT_BY_MONTH" == "split" ]]; then
+            echo "Split by Month mode. Iterate each month for season $SEASON"
+            for month in $(seq $((10#$FROM_MONTH)) $((10#$TO_MONTH))); do
+                local each_month=$(printf "%02d\n" $month)
+                local from_date="${YEAR}-${each_month}-01"
+                local each_month_last_day="$(get_last_day_of_month $each_month $YEAR)"
+                local to_date="${YEAR}-${each_month}-${each_month_last_day}"
+                osxphotos export $EXPORT_DIR \
+                    --from-date "${from_date}" \
+                    --to-date "${to_date}" \
+                    --directory "$OUTPUT_DIR_TEMPLATE" \
+                    --filename "$OUTPUT_NAME_TEMPLATE" \
+                    --edited-suffix "" \
+                    --skip-original-if-edited \
+                    --download-missing \
+                    --use-photokit \
+                    --retry 5 \
+                    --update \
+                    --cleanup \
+                    --report $REPORT_PATH
+            done
+        fi
     fi
     set +x
 } 
